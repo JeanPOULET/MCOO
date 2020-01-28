7a2dad0424bcae40d08e61e4b88f8ee3a43ed708
Nocturne-hub <jeremy-du25@hotmail.fr>
2020-01-28 09:01:49 +0100

    ajout

--- src/main/java/NonTerminal.java
+++ src/main/java/NonTerminal.java
@@ -1,3 +1,13 @@
+import com.sun.javafx.applet.ExperimentalExtensions;
+
 public abstract class NonTerminal implements Expression{
 
+    public void ajouter(Expression e){
+
+    }
+
+    public Expression supprimer(int i){
+        Expression e = null;
+        return e;
+    }
 }

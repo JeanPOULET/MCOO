6bdda7696a1932fcb96a97caa10fb1ee480a2b63
MathieuBart <mathieu.bart3@gmail.com>
2020-02-11 10:35:04 +0100

    oui

--- src/main/java/ExprFactory.java
+++ src/main/java/ExprFactory.java
@@ -1,4 +1,3 @@
-import sun.jvm.hotspot.oops.Instance;
 
 public class ExprFactory {
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.security.*;
-import java.util.*;
+
 
 public class Main {
 
@@ -9,13 +8,14 @@ public class Main {
         Expression X = f.makeLeaf("Litteral", "X");
         Expression[] nb = new Expression[4];
         for (int i=0; i < 4; i++) {
-            nb[i] = f.makeLeaf("Number", i);
+            nb[i] = f.makeLeaf("Entier", i);
         }
         Expression ens123 = f.makeNode("EnsembleEnExtension", nb[1], nb[2], nb[3]);
-        Expression exemple1 = f.makeNode("EtLogique",
-                f.makeNode("Egalite", X, ens123),
-                f.makeNode("QuelqueSoit", x, f.makeNode("Appartient", x, X), f.makeNode("Sup", x, nb[0]))
+        Expression exemple1 = f.makeNode("Conjonction",
+                f.makeNode("EgalArit", X, ens123),
+                f.makeNode("PourTout", x, f.makeNode("Appartient", x, X), f.makeNode("Superieur", x, nb[0]))
         );
+
 
     }
 
